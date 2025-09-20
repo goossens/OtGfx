6c2458fa332d41a0c2a38e9c225cd50596571d52
Johan Goossens <johan@cox.net>
2025-09-20 07:20:42 -0400

    Ported Map classes

---
+++ gfx/geo/OtSlippyMap.cpp
@@ -0,0 +1,81 @@
+//	ObjectTalk Scripting Language
+//	Copyright (c) 1993-2025 Johan A. Goossens. All rights reserved.
+//
+//	This work is licensed under the terms of the MIT license.
+//	For a copy, see <https://opensource.org/licenses/MIT>.
+
+
+//
+//  Include files
+//
+
+#include <list>
+#include <memory>
+
+#include "OtSlippyMap.h"
+
+
+//
+//	OtSlippyMap::render
+//
+
+void OtSlippyMap::render(const char* label, OtLatLon& center, double& resolution, ImVec2 size) {
+	// start a child window to enable clipping
+	ImGui::BeginChild("SlippyMap");
+
+	// use all available space by default
+	if (size.x == 0.0f || size.y == 0.0f) {
+		size = ImGui::GetContentRegionAvail();
+	}
+
+	// set default resolution (if required)
+	if (resolution == 0.0) {
+		resolution = 2.0 / static_cast<double>(size.x);
+	}
+
+	// setup screen context
+	ImVec2 pos = ImGui::GetCursorScreenPos();
+	ImDrawList* drawlist = ImGui::GetWindowDrawList();
+	ImGui::SetNextItemAllowOverlap();
+	ImGui::InvisibleButton(label, size);
+	ImGui::SetItemKeyOwner(ImGuiKey_MouseWheelY);
+
+	// handle user interactions
+	if (ImGui::IsItemHovered()) {
+		if (ImGui::IsMouseDoubleClicked(ImGuiMouseButton_Left)) {
+			center = projector.screenToLatLon(ImGui::GetMousePos());
+			resolution /= 2.0;
+
+		} else if (ImGui::IsMouseDragging(ImGuiMouseButton_Left)) {
+			auto raster = projector.latLonToRaster(center);
+			auto mouseDelta = ImGui::GetMouseDragDelta(ImGuiMouseButton_Left);
+
+			raster.x -= static_cast<double>(mouseDelta.x) * resolution;
+			raster.y -= static_cast<double>(mouseDelta.y) * resolution;
+
+			center = projector.rasterToLatLon(raster);
+			ImGui::ResetMouseDragDelta(ImGuiMouseButton_Left);
+
+		} else {
+			auto zoom = ImGui::GetIO().MouseWheel;
+
+			if (zoom != 0.0f) {
+				resolution *= 1.0f + zoom * 0.05f;
+			}
+		}
+	}
+
+	// update projector
+	projector.setWindowSize(size);
+	projector.setWindowOffset(pos);
+
+	projector.setCenter(center);
+	projector.setResolution(resolution);
+
+	// render map layers
+	for (auto& layer : layers) {
+		layer->render(drawlist, projector);
+	}
+
+	ImGui::EndChild();
+}
---
+++ gfx/geo/OtTileLayer.cpp
@@ -0,0 +1,104 @@
+//	ObjectTalk Scripting Language
+//	Copyright (c) 1993-2025 Johan A. Goossens. All rights reserved.
+//
+//	This work is licensed under the terms of the MIT license.
+//	For a copy, see <https://opensource.org/licenses/MIT>.
+
+
+//
+//  Include files
+//
+
+#include <vector>
+
+#include "fmt/format.h"
+
+#include "OtFramework.h"
+#include "OtMapTile.h"
+#include "OtTileLayer.h"
+
+
+//
+//  OtTileLayer::render
+//
+
+void OtTileLayer::render(ImDrawList* drawlist, OtProjector& projector) {
+	// determine edges of visible map portion in raster coordinates
+	OtMapRaster minR, maxR;
+	projector.visibleRasterBoundaries(minR, maxR);
+
+	// determine visible tiles
+	std::vector<std::shared_ptr<OtMapImageTile>> tiles;
+	bool done = false;
+
+	// process all zoom levels until a complete (opaque) level is found
+	for (int c = projector.getZoom(); c > 0 && !done; c--) {
+		// get extent of visible tiles
+		OtMapTile minT = projector.rasterToTile(minR, c);
+		OtMapTile maxT = projector.rasterToTile(maxR, c);
+
+		// see if a layer is complete
+		bool incomplete = false;
+
+		// process all visible tiles
+		for (int x = minT.x; x <= maxT.x; x++) {
+			for (int y = minT.y; y <= maxT.y; y++) {
+				std::shared_ptr<OtMapImageTile> image = getTile(OtMapTile(c, x, y));
+				tiles.push_back(image);
+				incomplete |= !image->isOpaque();
+			}
+		}
+
+		done = !incomplete;
+	}
+
+	// render all visible tiles (in reverse order)
+	for (auto i = tiles.rbegin(); i != tiles.rend(); i++) {
+		(*i)->render(drawlist, projector);
+	}
+
+	// render copyright notice
+	static constexpr const char* copyright = "© OpenStreetMap";
+	static constexpr float margin = 3.0f;
+
+	auto windowSize = projector.getWindowSize();
+	auto windowOffset = projector.getWindowOffset();
+	auto textSize = ImGui::CalcTextSize(copyright) + ImVec2(margin * 2.0f, margin * 2.0f);
+	ImVec2 textOffset = windowOffset + windowSize - textSize;
+
+	auto pos = ImGui::GetCursorScreenPos();
+	ImGui::SetCursorScreenPos(textOffset);
+
+	if (ImGui::InvisibleButton("Copyright", textSize)) {
+		OtFramework::openURL("https://www.openstreetmap.org/copyright");
+	}
+
+	drawlist->AddRectFilled(textOffset, windowOffset + windowSize, IM_COL32(255, 255, 255, 160));
+	drawlist->AddText(textOffset + ImVec2(margin, margin), IM_COL32(0, 0, 238, 255), copyright);
+
+	// DUmmy is require to avoid Dear ImGui error (see https://github.com/ocornut/imgui/issues/5548)
+	ImGui::SetCursorScreenPos(pos);
+	ImGui::Dummy(ImVec2());
+}
+
+
+//
+//  OtTileLayer::getTile
+//
+
+std::shared_ptr<OtMapImageTile> OtTileLayer::getTile(const OtMapTile& tile) {
+	// see if tile is in memory cache
+	std::shared_ptr<OtMapImageTile> imageTile;
+
+	if (cache.has(tile.hash)) {
+		imageTile = cache.get(tile.hash);
+
+	} else {
+		// no, load new tile
+		auto url = fmt::format("http://{}/{}/{}/{}.png", host, tile.zoom, tile.x, tile.y);
+		imageTile = std::make_shared<OtMapImageTile>(tile, url);
+		cache.set(tile.hash, imageTile);
+	}
+
+	return imageTile;
+}
